e0e6e00b6f54284d61cd84d29d3cf11043b6ad18
ksdshpx <ksdshpx2018@aliyun.com>
2020-01-21 10:19:26 +0800

    PC寄存器的测试！
    PC寄存器的测试！

---
+++ src/test/java/cn/ksdshpx/interview/jvm/PCRegisterTest.java
@@ -0,0 +1,19 @@
+package cn.ksdshpx.interview.jvm;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.xing
+ * Date: 2020/1/21
+ * Time: 9:42
+ * Description:PC寄存器的测试
+ */
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
