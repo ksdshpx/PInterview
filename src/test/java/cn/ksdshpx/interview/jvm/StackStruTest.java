c2508d3b4f480399061c314b8f70155e459ba0a7
peng.xing <southeast_px@163.com>
2020-02-05 22:46:24 +0800

    栈结构演示！
    栈结构演示！

---
+++ src/test/java/cn/ksdshpx/interview/jvm/StackStruTest.java
@@ -0,0 +1,17 @@
+package cn.ksdshpx.interview.jvm;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2020/2/4
+ * Time: 20:29
+ * Description:栈结构演示
+ */
+public class StackStruTest {
+    public static void main(String[] args) {
+        //int i = 2 + 3;
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+    }
+}
